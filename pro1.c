7de5147165e443fb1fc4fe2e7fc5c215ec8b490e
Vignesh K Palani <45666028+Vignesh-K-Palani@users.noreply.github.com>
2019-07-15 00:08:01 +0530

    Update pro1.c

--- pro1.c
+++ pro1.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
-void main(){
-  int num, flag = 0, i, j, k = 0;
+int main(){
+  int num, flag = 0, i, j = 0, k = 0;
   scanf("%d", &num);
-  char arr[num][100000], result[100000]='\0';
-  for(int i=0; i<100000; i++){
+  int count[num];
+  char arr[num][10], result[10], ch;
+  scanf("%c", &ch);
+  for(i=0; i<num; i++){
+    while(ch != '\0'){
+      arr[i][j] = ch;
+      j++;
+      scanf("%c", &ch);
+    }
+    count[i] = j;
+  }
+  for(int i=0; i<count[i]; i++){
     for(int j=0; j<num-1; j++){
       if(arr[j][i] == arr[j+1][i])
         flag = 1;
@@ -20,4 +30,5 @@ void main(){
       break;
   }
   printf("%s", result);
+  return 0;
 }
